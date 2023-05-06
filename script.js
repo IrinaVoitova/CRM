6bc68983be7fb1450d260a191af3b8e7b0a32905
Irina Voitova <iravoitova1994@gmail.com>
2023-05-06 15:18:31 +0300

    CRM js

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let productName = 'Томат';
+let productAmount = 2;
+let productCategory = 'Овощи';
+let productPrice = 200;
+const productSum = productAmount * productPrice;
+
+console.log(productName);
+console.log(productSum, "рублей");
