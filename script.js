17d9f87c9369e71568006f4f8185a6d28c0e1a76
Irina Voitova <iravoitova1994@gmail.com>
2023-05-09 21:59:53 +0300

    prompt

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ const productSum = productAmount * productPrice;
 
 console.log(productName);
 console.log(productSum, "рублей");
+
+
+const productNameUser = prompt('Наименование товара', );
+console.log(typeof productNameUser);
+
+const productAmountUser = (prompt('Количество товара', )) * 1;
+console.log(typeof productAmountUser);
+
+const productCategoryUser = prompt('Категория товара', );
+console.log(typeof productCategoryUser);
+
+const productPriceUser = (prompt('Цена товара', )) * 1;
+console.log(typeof productPriceUser);
+
+const productSumUser = productAmountUser * productPriceUser;
+
+console.log(`На складе ${productAmountUser} единицы товара "${productNameUser}" на сумму ${productSumUser} рублей`);
