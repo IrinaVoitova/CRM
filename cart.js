ce0bad75531ebf7a08920d9a80ccfda8d6d5eb06
Irina Voitova <iravoitova1994@gmail.com>
2023-05-31 15:50:39 +0300

    4.10

--- cart.js
+++ cart.js
@@ -2,72 +2,71 @@
 
 
 const cart = {
-    items: [],
-    totalPrice: 0,
-    counts: 0,
-    
-    clear() {
-        this.items = [];
-        this.totalPrice = 0;
-        this.counts = 0;
-        return cart
-    },
+  items: [],
+  totalPrice: 0,
+  counts: 0,
 
-    Add(name, price, count) {
-        const obj = {
-        name: name,
-        price: price,
-        count: count ?? '1',
-        };
-        this.items.push(obj)
-    },
+  clear() {
+    this.items = [];
+    this.totalPrice = 0;
+    this.counts = 0;
+    return cart;
+  },
 
-    calculateItemPrice() {
-        let resultPrice = [] = this.items.map(item => item.price);
-        console.log(resultPrice);
-        let resultCount = [] = this.items.map(item => item.count);
-        console.log(resultCount);
-        const resultSum = [];
-        for (let i = 0; i < resultPrice.length; i++) {
-            const a = resultPrice[i];
-            const b = resultCount[i];
-                resultSum.push(a * b)
-        }
-        const calculatemPrice = resultSum.reduce((acc, item) => {
-        return acc + item
-        })
-        return calculatemPrice
-    },
+  add(name, price, count) {
+    const obj = {
+      name,
+      price,
+      count: count ?? '1',
+    };
+    this.items.push(obj);
+    return obj;
+  },
 
-    increaseCount(z) {
-        let resultCount2 = [] = this.items.map(item => item.count);
-        console.log(resultCount2);
-        const calculatemPrice2 = resultCount2.reduce((acc, item) => {
-            return Number(acc) + Number(item)
-        })
-        return calculatemPrice2 + (z ?? 0)
-    },
-    print() {
-        const ItemsStr = JSON.stringify(this.items)
-        console.log(ItemsStr)
-        console.log(`Общая стоимость корзины составляет ${this.totalPrice} рублей`)
-    },
+  calculateItemPrice() {
+    const resultPrice = [] = this.items.map(item => item.price);
+    console.log(resultPrice);
+    const resultCount = [] = this.items.map(item => item.count);
+    console.log(resultCount);
+    const resultSum = [];
+    for (let i = 0; i < resultPrice.length; i++) {
+      const a = resultPrice[i];
+      const b = resultCount[i];
+      resultSum.push(a * b);
+    }
+    const calculatemPrice = resultSum.reduce((acc, item) => acc + item);
+    return calculatemPrice;
+  },
+
+  increaseCount(z) {
+    const resultCount2 = [] = this.items.map(item => item.count);
+    console.log(resultCount2);
+    const calculatemPrice2 = resultCount2.reduce((acc, item) =>
+      Number(acc) + Number(item));
+    return calculatemPrice2 + (z ?? 0);
+  },
+  print() {
+    const ItemsStr = JSON.stringify(this.items);
+    console.log(ItemsStr);
+    console.log(`Общая стоимость корзины составляет ${this.totalPrice} рублей`);
+  },
 };
 
-const obj1 = cart.Add("томат", "16");
-const obj2 = cart.Add("яблоко", "9", "10");
-const obj3 = cart.Add("апельсин", "20", "1");
-const obj4 = cart.Add("огурец", "50", "12");
-console.log(obj1);
+
+const obj1 = cart.add('томат', '16');
+const obj2 = cart.add('яблоко', '9', '10');
+const obj3 = cart.add('апельсин', '20', '1');
+const obj4 = cart.add('огурец', '50', '12');
+console.log(obj1, obj2, obj3, obj4);
 console.log(cart.items);
-console.log(cart.totalPrice);
+// console.log(cart.totalPrice);
 cart.counts = cart.increaseCount();
 console.log(cart.counts);
 Object.defineProperty(cart, 'totalPrice', {
-    get() {
-        return cart.calculateItemPrice()
-    },
-    set() {}
+  get() {
+    return cart.calculateItemPrice();
+  },
+  set() {},
 });
 cart.totalPrice = 100;
 cart.print();
